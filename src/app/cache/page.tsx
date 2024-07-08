c33132a25def99b3702596e21b9f74d13ceffbfa
MANCHALA SREEKANTH <76148957+odysseus1999@users.noreply.github.com>
2024-07-08 10:07:17 +0530

    Update page.tsx

--- src/app/cache/page.tsx
+++ src/app/cache/page.tsx
@@ -3,14 +3,5 @@ import { getAllPostsFromNotion } from '@/services/posts';
 export default async function CachePage() {
   const allPosts = await getAllPostsFromNotion();
 
-  return (
-    <div id="posts">
-      {allPosts.map((post) => (
-        <div key={post.id}>
-          <h2>{post.title}</h2>
-          {/* Render other post details here */}
-        </div>
-      ))}
-    </div>
-  );
+  return <div id="posts" data-posts={JSON.stringify(allPosts)} />;
 }
